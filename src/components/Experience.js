242f833b7e432fc96fb958adf71b235f11b364db
BMoClay <benmorgancleveland@gmail.com>
2021-04-28 16:17:25 -0400

    experience component completed git checkout -b experience

---
+++ src/components/Experience.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Experience = () => {
+    return (
+        <div className="experience">
+            <div className="d-flex justify-content-center my-5">
+                <h1>experience</h1>
+            </div>
+            <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2008-2018</h3>
+                        <p>real fine arts was a gallery that I ran in brooklyn during this period</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2017-2020</h3>
+                        <p>Bonsai fine arts fine arts technician</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2019-2019</h3>
+                        <p>pratt institute visiting professor</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2012-2015</h3>
+                        <p>Seth Price artists assistant</p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2008-2018</h3>
+                        <p>real fine arts was a gallery that I ran in asdfasdfasdf brooklyn during this period</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Experience
