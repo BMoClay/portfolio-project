af9320310945b7a2915e97ebb94c3d5d7225e53c
BMoClay <benmorgancleveland@gmail.com>
2021-05-23 12:16:07 -0400

    install react-responsive-carousel

---
+++ src/components/Testimonials.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Testimonials = () => {
+    return (
+        <div className="testimonials">
+            <h1>my happy clients</h1>
+            <div className="testimonials-content"></div>
+            
+        </div>
+    )
+}
+
+export default Testimonials
