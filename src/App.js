a4b9b89895bd4ffc6b7143e721067c6c9b5e1c92
BMoClay <benmorgancleveland@gmail.com>
2021-05-31 15:46:15 -0400

    fix bug in contacts component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import AboutMe from './components/AboutMe';
 import Services from './components/Services';
 import Experience from './components/Experience';
 import Portfolio from './components/Portfolio';
-import Testimonials from './components/Testimonials'
+import Testimonials from './components/Testimonials';
+import Contacts from "./components/Contacts";
 
 
 function App() {
@@ -42,6 +43,7 @@ function App() {
       <Experience/>
       <Portfolio/>
       <Testimonials/>
+      <Contacts/>
     </>
   );
 }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
-import { useForm } from 'react-hook-form';
+import React, {useState} from "react";
+import emailjs from "emailjs-com";
+import { useForm } from "react-hook-form";
 
-
-const Contacts = () => {
-    const [successMessage, setSuccessMessage] = useState("");
-    const { register, handleSubmit, errors } = useForm();
+function Contact(){
+    const [successMessage, setSuccessMessage] = useState("")
+    const {register, handleSubmit, formState: { errors }} = useForm();
+    // const {register, handleSubmit, errors} = useForm();
 
     const serviceID = "service_ID";
     const templateID = "template_ID";
     const userID = "user_6DEGhl4tKlbhvoVlmgp8J";
 
-    const onSubmit = (data, r) => {
+    function onSubmit (data,r) {
         sendEmail(
             serviceID,
             templateID,
@@ -20,103 +20,154 @@ const Contacts = () => {
                 phone: data.phone,
                 email: data.email,
                 subject: data.subject,
-                description: data.description
+                message: data.message
             },
             userID
         )
-        r.target.reset();
+        r.target.reset()
     }
 
-    const sendEmail = (serviceID, templateID, variables, userID) => {
+
+    function sendEmail(serviceID,templateID , variables, userID) {
+       
+    
         emailjs.send(serviceID, templateID, variables, userID)
           .then(() => {
-              setSuccessMessage("Form sent successfully! I'll get back to you ASAP");
-          }).catch(err => console.error(`Something went wrong :/ ${err}`))
+            setSuccessMessage("Form sent successfully!");
+          }).catch(err => console.error(`Something when wrong ${err}`));
+      
       }
     
-    return (
-        <div className="contacts">
-            <div className="text-center">
-                <h1>contact me</h1>
-                <p>please fill out the form and describe your project and needs 
-                    and I'll get back to you ASAP
-                </p>
+    return(
+        <div id = "contact" className= "contact">
+            <div className = "text-center">
+                <h1> Contact Me</h1>
+                <p>Please provide your details below</p>
+                <span className = "success-message">{successMessage}</span>
             </div>
-            <div className="container">
-                <form onSubmit={handleSubmit(onSubmit)}>    
-                <div className="row">
-                    <div className="col-md-6 col-xs-12">
-                        {/* NAME INPUT */}
-                        <div className="text-center">
-                            <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Name"
-                            name="name"
-                            ref={
-                                register({
-                                    required: "please enter your name",
-                                    maxLength: {
-                                        value: 20,
-                                        message: "Please enter a name w fewer than 25 characters"
-                                    }
-                                })
-                            }
-                            />
-                            <div className="line"></div>
+            <div className = "container">
+                    <form onSubmit = {handleSubmit(onSubmit)}>
+                        <div className= "row">
+                            <div className = "col-md-6 col xs-12">
+                                {/* Name Input */}
+                                <div className = "text-center">
+                                <input 
+                                    type = "text"
+                                    className = "form-control"
+                                    placeholder = "Name"
+                                    name = "name"
+                                    {...register('name', {
+                                        max: {
+                                            value: 25,
+                                            message: 'please enter name w fewer than 25 characters'
+                                        }
+                                    })}
+                                    // {...register({
+                                    //             required: "Please enter your name",
+                                    //             maxLength: {
+                                    //                 value: 25,
+                                    //                 message: "Please enter a name with fewer than 25 characters"
+                                    //             }})}
+                                    // ref = {
+                                    //     register({
+                                    //         required: "Please enter your name",
+                                    //         maxLength: {
+                                    //             value: 25,
+                                    //             message: "Please enter a name with fewer than 25 characters"
+                                    //         }
+                                    //     })
+                                    // }
+                                    />
+                                <div className = "line"></div>
+                                    </div>
+                                    <span className = "error-messages">
+                                        {errors.name && errors.name.message}
+                                    </span>
+                                <div className = "text-center">
+
+                                {/* Email Input */}
+                                <input 
+                                    type = "email"
+                                    className = "form-control"
+                                    placeholder = "Email"
+                                    name = "email"
+                                    {...register('value_name')}
+                                    // ref = {
+                                    //     register({
+                                    //         required: "Please add your email",
+                                    //         pattern: {
+                                    //             value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    //             message: "invalid email"
+                                    //         }
+                                    //     })
+                                    // }
+                                    />
+                                <div className = "line"></div>
+                                    </div>
+                                    <span className = "error-messages">
+                                        {errors.email && errors.email.message}
+                                    </span>
+                                {/* Phone Input */}
+                                <div className = "text-center">
+                                    <input 
+                                        type = "text"
+                                        className = "form-control"
+                                        placeholder = "Phone Number"
+                                        name = "phone"
+                                    />
+                                    <div className = "line"></div>
+                                </div>
+                                {/* Subject Input */}
+                                <div className = "text-center">
+
+                                    <input 
+                                        type = "text"
+                                        className = "form-control"
+                                        placeholder = "Subject"
+                                        name = "subject"
+                                        {...register('value_name')}
+                                        // ref = {
+                                        //     register({
+                                        //         required: "Please provide a subject"
+                                        //     })
+                                        // }
+                                    />
+                                <div className = "line"></div>
+                            </div>
+                            <span className = "error-messages">
+                                        {errors.subject && errors.subject.message}
+                                    </span>
                         </div>
-                        <span className="error-message">
-                            {errors.name && errors.name.message}
-                        </span>
-                        {/* PHONE INPUT */}
-                        <div className="text-center">
-                            <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Phone"
-                            name="phone"
-                            />
-                            <div className="line"></div>
-                        </div>
-                        {/* Email INPUT */}
-                        <div className="text-center">
-                            <input
-                            type="email"
-                            className="form-control"
-                            placeholder="Email"
-                            name="email"
-                            />
-                            <div className="line"></div>
-                        </div>
-                        {/* Subject INPUT */}
-                        <div className="text-center">
-                            <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Subject"
-                            name="subject"
-                            />
-                            <div className="line"></div>
-                        </div>
+                            {/* Subject Input */}
+                            <div className = "col-md-6 col-xs-12">
+                            <div className = "text-center">
+
+                                    <textarea 
+                                        type = "text"
+                                        className = "form-control"
+                                        placeholder = "Message"
+                                        name = "message" 
+                                        {...register('value_name')}
+                                        // ref = {
+                                        //     register({
+                                        //         required: "Please provide what this is in regards to"
+                                        //     })
+                                        // }
+                                        >
+                                    </textarea>
+                                <div className = "line"></div>
+                            </div>
+                            <span className = "error-messages">
+                                        {errors.message && errors.message.message}
+                                    </span>
+                                <button className = "btn btn-primary" type = "submit">Contact me</button>
+                            </div>
                     </div>
-                    <div className="col-md-6 col-xs-12">
-                        {/* DESCRIPTION */}
-                        <div className="text-center">
-                            <textarea
-                            type="text"
-                            className="form-control"
-                            placeholder="please briefly describe your project..."
-                            name="description"
-                            ></textarea>
-                            <div className="line"></div>
-                        </div>
-                        <button className="btn-main-offer contact-btn" type="submit">contact me</button>
-                    </div>
-                </div>
-            </form>
+                </form>
             </div>
         </div>
     )
+
 }
 
-export default Contacts
+export default Contact;
