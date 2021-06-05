53eb509828ca54509d28aa66e4860a36a83ac5ad
BMoClay <benmorgancleveland@gmail.com>
2021-06-05 13:33:10 -0400

    fix contact button in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ const Header = () => {
     return (
       <div id="home" className='header-wrapper'>
           <div className="main-info">
-                {/* <h1>Ben Morgan-Cleveland</h1> */}
                 <h1>Full Stack Web Developer</h1>
                 <Typed
                     className='typed-text'
