2ae4e7d05cd431b56f002b099cbd41abd5be494c
BMoClay <benmorgancleveland@gmail.com>
2021-06-07 22:44:39 -0400

    got master back to where it should be

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Typed from 'react-typed';
+import {Link} from "react-scroll";
+
+
+const Header = () => {
+    return (
+      <div id="home" className='header-wrapper'>
+          <div className="main-info">
+                {/* <h1>Ben Morgan-Cleveland</h1> */}
+                <h1>Full Stack Web Developer</h1>
+                <Typed
+                    className='typed-text'
+                    strings={['React', 'Redux', 'Rails', 'Javascript', 'PostgresQL', 'Firebase', 'Git']}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                {/* <a href="#" className="btn-main-info">contact me</a> */}
+                <Link className='btn-main-info' smooth={true} to="contact" href="#">Contact Me</Link>
+          </div>
+      </div>
+    )
+}
+
+export default Header
