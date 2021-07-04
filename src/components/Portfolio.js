592e953e5a0c824352ed83b7b5faa9adf33f7c94
BMoClay <benmorgancleveland@gmail.com>
2021-07-04 19:25:13 -0400

    added raregifs

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import iSherpaPic from '../project_images/iSherpa_screenshot.png';
+import RaregifsPic from '../project_images/screengrab_raregifs.png';
 // FONTAWESOME IMPORTS
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
@@ -37,6 +38,41 @@ const Portfolio = () => {
         fadeIn: true,
         fadeInSpeed: 500
     }
+
+    
+
+
+    const openPopupboxRaregifs = () => {
+        const content = (
+            <>
+                <img classname="portfolio-image-popupbox" src={RaregifsPic} alt="Raregifs Project"/>
+                <p>Raregifs is a place to create and share animated digital sketches</p>
+                <b>Demo:</b> 
+                <a className="hyper-link" 
+                    onClick={() => window.open("https://www.loom.com/share/5a55e35216e54a198e4cf083a35c739c", "_blank")}>
+                    https://www.loom.com/share/5a55e35216e54a198e4cf083a35c739c</a>
+                <br></br>
+                <b>Github:</b> 
+                <a className="hyper-link" 
+                    onClick={() => window.open("https://github.com/BMoClay/Raregifs-front-end")}>
+                        https://github.com/BMoClay/Raregifs-front-end</a>
+            </>
+        )
+        PopupboxManager.open({content})
+    }
+
+    const popupboxConfigRaregifs = {
+        titleBar: {
+            enable: true,
+            text: "Raregifs project"
+        },
+        fadeIn: true,
+        fadeInSpeed: 500
+    }
+
+
+
+
     return (
         <div id="portfolio" className="portfolio-wrapper">
             <div className="container">
@@ -47,9 +83,15 @@ const Portfolio = () => {
                         <div className="overflow"></div>
                         <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
                     </div>
+                    <div className="portfolio-image-box" onClick={openPopupboxRaregifs}>
+                        <img className="portfolio-image" src={RaregifsPic} alt="Raregifs Project"/>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+                    </div>
                 </div>
             </div>
             <PopupboxContainer {...popupboxConfigiSherpa} />
+            <PopupboxContainer {...popupboxConfigRaregifs} />
         </div>
     )
 }
