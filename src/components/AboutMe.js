b10dddaa4ea35b27516b3026a621a72a9d08619b
BMoClay <benmorgancleveland@gmail.com>
2021-07-14 16:48:48 -0400

    improve Services css

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,29 +3,27 @@ import Author from '../profile-pic.jpg';
 
 const AboutMe = () => {
     return (
-        <div id="about" className="container py-5">
-            <div className="row">
-               <div className="col-lg-6 col-xm-12">
-                    <div className="photo-wrap mb-5">
-                        <img className="profile-pic"  src={Author} alt="Ben Morgan-Cleveland profile pic"/>        
+        // <div id="about" className="container py-5">
+        <div id="about">
+            <div className="container py-5">
+                <div className="row">
+                    <div className="col-lg-6 col-xm-12">
+                        <div className="photo-wrap mb-5">
+                            <img className="profile-pic"  src={Author} alt="Ben Morgan-Cleveland profile pic"/>        
+                        </div>
                     </div>
-                </div>
-               <div className="col-lg-6 col-xm-12">
-                <h1 className="about-heading">about me</h1>
-                    <p>
-                        I'm a Full stack web developer based in New York. 
-                        Experienced in Ruby on Rails and 
-                        JavaScript based programming with 
-                        a background in small business ownership.
-                        Highly skilled in React and Redux.
-                        Possess exceptional skills in communication,
-                        team-building and project management that 
-                        helped deliver sales and press at New York
-                        art gallery. 
-                        {/* Outstanding team player 
-                        focused on achieving project objectives. Eager to enhance 
-                        a company or individual's user experience. */}
-                    </p>
+                    <div className="col-lg-6 col-xm-12">
+                        <h1 className="about-heading">about me</h1>
+                            <p>
+                                I'm a Full stack web developer based in New York
+                                with experience in Ruby on Rails and 
+                                JavaScript based programming such as React. My projects incorporate front-end and back-end development with robust user interfaces that use efficient code to solve real world problems. 
+                                I'm an open minded, innovative thinker with a
+                                background in fine arts and small business ownership. My other skills 
+                                include project managment, team building, debugging and
+                                code optimization.
+                            </p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -5,51 +5,33 @@ import { faDesktop, faFileCode } from '@fortawesome/free-solid-svg-icons';
 
 const Services = () => {
     return (
-        <div id="services" className="services">
-            <h1 className="py-5">my services</h1>
-                <div className="container">
-                    <div className="row" >
-                        <div className="col-lg-6 col-md-6 col-sm-6" >
-                            <div className="box">
-                                <div className='circle'>
-                                    <FontAwesomeIcon className="icon" icon={faDesktop} size='2x'/>
+        <div id="services" className="services-wrapper">
+            <div className="container">
+                <h1 className="text-uppercase text-center py-5">services</h1>
+                    <div className="container">
+                        <div className="row" >
+                            <div className="col-lg-6 col-md-6 col-sm-6" >
+                                <div className="box">
+                                    <div className='circle'>
+                                        <FontAwesomeIcon className="icon" icon={faDesktop} size='2x'/>
+                                    </div>
+                                    <h3>Web Design</h3>
+                                    <p>Approach each project individually with a focus on results.
+                                        Utilizing Bootstrap, Material UI, and Custom CSS.
+                                    </p>
                                 </div>
-                                <h3>Web Design</h3>
-                                <p>Approach each project individually with a focus on results.
-                                    Utilizing Bootstrap, Material UI, Custom CSS, HTML.
-                                </p>
+                            </div>
+                            {/* - */}
+                            <div className="col-lg-6 col-md-6 col-sm-6">
+                                <div className="box">
+                                    <div className='circle'>
+                                        <FontAwesomeIcon className="icon" icon={faFileCode} size='2x'/>
+                                    </div>
+                                    <h3>Web Development</h3>
+                                    <p>Implement new proven technologies such as React, Firebase, and Ruby on Rails, for an efficient, readable backend.</p>
+                                </div>
                             </div>
                         </div>
-                        {/* - */}
-                        <div className="col-lg-6 col-md-6 col-sm-6">
-                            <div className="box">
-                                <div className='circle'>
-                                    <FontAwesomeIcon className="icon" icon={faFileCode} size='2x'/>
-                                </div>
-                                <h3>Web Development</h3>
-                                <p>Implement new proven technologies such as React, Redux, Firebase, and Ruby on Rails, for an efficient, readable backend.</p>
-                            </div>
-                        </div>
-                        {/* - */}
-                        {/* <div className="col-lg-3 col-md-6 col-sm-6">
-                            <div className="box">
-                                <div className='circle'>
-                                    <FontAwesomeIcon className="icon" icon={ faFacebookF } size='2x'/>
-                                </div>
-                                <h3>Facebook ads SMM</h3>
-                                <p>Potential clients will see your services or product on Facebook</p>
-                            </div>
-                        </div> */}
-                        {/* - */}
-                        {/* <div className="col-lg-3 col-md-6 col-sm-6">
-                            <div className="box">
-                                <div className='circle'>
-                                    <FontAwesomeIcon className="icon" icon={ faGoogle } size='2x'/>
-                                </div>
-                                <h3>Google Ads</h3>
-                                <p>Your services or products will appear at the top of google search results</p>
-                            </div>
-                        </div> */}
                     </div>
                 </div>
             </div>
